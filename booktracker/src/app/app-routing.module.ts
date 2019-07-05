ae6065be2bfe4488ea47fab1157ff02c2d9cf8f3
Brice Wilson <brice@5wilsons.net>
2019-07-05 14:53:38 -0400

    code for feature module lab

--- booktracker/src/app/app-routing.module.ts
+++ booktracker/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AddBookComponent } from "src/app/books/add-book/add-book.component";
-import { AddReaderComponent } from "src/app/add-reader/add-reader.component";
+import { AddReaderComponent } from "src/app/readers/add-reader/add-reader.component";
 import { DashboardComponent } from "src/app/dashboard/dashboard.component";
 import { EditBookComponent } from "src/app/books/edit-book/edit-book.component";
-import { EditReaderComponent } from "src/app/edit-reader/edit-reader.component";
+import { EditReaderComponent } from "src/app/readers/edit-reader/edit-reader.component";
 import { BooksResolverService } from './core/books-resolver.service';
 
 const routes: Routes = [
--- booktracker/src/app/app.module.ts
+++ booktracker/src/app/app.module.ts
@@ -3,20 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AddReaderComponent } from './add-reader/add-reader.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditReaderComponent } from './edit-reader/edit-reader.component';
 import { BooksModule } from './books/books.module';
 import { BookLibraryModule } from 'book-library';
+import { ReadersModule } from './readers/readers.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    EditReaderComponent,
-    AddReaderComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +22,9 @@ import { BookLibraryModule } from 'book-library';
     FormsModule,
     HttpClientModule,
     BooksModule,
-    BookLibraryModule
+    BookLibraryModule,
+    ReadersModule,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
