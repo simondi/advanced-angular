937265ce1242a3785724457260f9daa75e4dc973
Brice Wilson <brice@5wilsons.net>
2019-07-09 16:03:39 -0400

    removed unused imports

--- booktracker/src/app/dashboard/dashboard.component.ts
+++ booktracker/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
