ba8e831992a877cbbbb84b38416b7d66d35686e9
Brice Wilson <brice@5wilsons.net>
2019-09-18 21:08:59 -0400

    updated variable name

--- booktracker/src/app/core/book_tracker_operators.ts
+++ booktracker/src/app/core/book_tracker_operators.ts
@@ -4,7 +4,7 @@ import { tap } from "rxjs/operators";
 import { Book } from "../models/book";
 
 export function logNewerBooks(year: number) {
-  return ($source: Observable<Book[]>) => $source.pipe(
+  return (source$: Observable<Book[]>) => source$.pipe(
     tap(books => books.forEach(b => b.publicationYear > year ? console.log(b) : null))
   );
 }
