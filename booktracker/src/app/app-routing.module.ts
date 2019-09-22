c99fad507f716f1e0025303ffb857806ab755028
Brice Wilson <brice@5wilsons.net>
2019-09-22 08:13:18 -0400

    moved routes to feature modules

--- booktracker/src/app/app-routing.module.ts
+++ booktracker/src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AddBookComponent } from "src/app/books/add-book/add-book.component";
-import { AddReaderComponent } from "src/app/readers/add-reader/add-reader.component";
 import { DashboardComponent } from "src/app/dashboard/dashboard.component";
-import { EditBookComponent } from "src/app/books/edit-book/edit-book.component";
-import { EditReaderComponent } from "src/app/readers/edit-reader/edit-reader.component";
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'addbook', component: AddBookComponent },
-  { path: 'addreader', component: AddReaderComponent },
-  { path: 'editreader/:id', component: EditReaderComponent },
-  { path: 'editbook/:id', component: EditBookComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
--- booktracker/src/app/readers/readers-routing.module.ts
+++ booktracker/src/app/readers/readers-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddReaderComponent } from './add-reader/add-reader.component';
+import { EditReaderComponent } from './edit-reader/edit-reader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addreader', component: AddReaderComponent },
+  { path: 'editreader/:id', component: EditReaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
