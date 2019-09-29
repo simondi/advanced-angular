653e178d3d549ac021b0bc98d538b2b12332c02f
Brice Wilson <brice@5wilsons.net>
2019-09-29 14:57:30 -0400

    code formatting

--- booktracker/src/app/books/state/book.reducer.ts
+++ booktracker/src/app/books/state/book.reducer.ts
@@ -1,8 +1,12 @@
 export function reducer(state, action) {
+
   switch (action.type) {
+
     case 'UPDATE_FAVORITE_BOOK':
+
       console.log('existing state: ' + JSON.stringify(state));
       console.log(action.payload);
+      
       return {
         ...state,
         favoriteBook: action.payload
